282de9f459ba0e161f49fb1dd46c6a293f5ee29e
ashoulson <ashoulson@gmail.com>
2015-09-09 22:37:38 -0400

    Forking, Pruning, Converting to Windows Forms
    Non-functional in its current state.

---
+++ ToggleMic/Interface.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ToggleMic
+{
+  partial class Interface
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.Status = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // Status
+      // 
+      this.Status.AutoSize = true;
+      this.Status.Location = new System.Drawing.Point(12, 9);
+      this.Status.Name = "Status";
+      this.Status.Size = new System.Drawing.Size(37, 13);
+      this.Status.TabIndex = 0;
+      this.Status.Text = "Status";
+      this.Status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.Status.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // Interface
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(202, 33);
+      this.Controls.Add(this.Status);
+      this.Name = "Interface";
+      this.Text = "CrappyPTT 1.0";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label Status;
+  }
+}
---
+++ ToggleMic/MicController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace ToggleMic
+{
+  public class MicController
+  {
+    private KeyboardHook keyboardHook = new KeyboardHook();
+
+    public MicController()
+    {
+      this.keyboardHook.KeyPressed += this.KeyboardHook_KeyPressed;
+      this.Mute(Keyboard.IsKeyToggled(Key.Scroll) == false);
+      this.IsActive = (this.GetActiveDevice().IsMuted == false);
+    }
+
+    private void KeyboardHook_KeyPressed(
+      object sender, 
+      KeyPressedEventArgs e)
+    {
+      switch (e.KeyPressed)
+      {
+        case Key.Scroll:
+          this.Mute(Keyboard.IsKeyToggled(Key.Scroll));
+          break;
+      }
+    }
+
+    private void Mute(bool mute)
+    {
+      CoreAudioDevice micDevice = this.GetActiveDevice();
+      micDevice.Mute(mute);
+      this.IsActive = (micDevice.IsMuted == false);
+    }
+
+    public bool IsActive { get; set; }
+
+    private CoreAudioDevice GetActiveDevice()
+    {
+      return
+        this.controller.GetCaptureDevices(DeviceState.Active)
+          .FirstOrDefault(o => o.IsDefaultDevice);
+    }
+
+    private CoreAudioController controller = new CoreAudioController();
+  }
+}
